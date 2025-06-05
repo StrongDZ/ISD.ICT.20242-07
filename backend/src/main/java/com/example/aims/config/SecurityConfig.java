ff8bfabf9f48821baff7bd73ef897e8419b43f82
Doan Quyen <thuquyentntl@gmail.com>
2025-06-05 18:45:07 +0700

    update securitt

--- backend/src/main/java/com/example/aims/config/SecurityConfig.java
+++ backend/src/main/java/com/example/aims/config/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import lombok.RequiredArgsConstructor;
 import com.example.aims.config.CustomizeRequestFilter;
-import com.example.aims.service.user.UserServiceDetail;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -37,15 +37,15 @@ public class SecurityConfig {
     // }
 
     private final CustomizeRequestFilter requestFilter;
-    private final UserServiceDetail userServiceDetail;
+    //private final UserServiceDetail userServiceDetail;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request.requestMatchers("/**").permitAll()
                         .anyRequest().authenticated())
-                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
-                .authenticationProvider(authenticationProvider()).addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
+                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS));
+      //          .authenticationProvider(authenticationProvider()).addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -59,13 +59,13 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setPasswordEncoder(passwordEncoder());
-        authProvider.setUserDetailsService(userServiceDetail.userService());
-        return authProvider;
-    }
+    // @Bean
+    // public AuthenticationProvider authenticationProvider() {
+    //     DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    //     authProvider.setPasswordEncoder(passwordEncoder());
+    //     authProvider.setUserDetailsService(userServiceDetail.userService());
+    //     return authProvider;
+    // }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
